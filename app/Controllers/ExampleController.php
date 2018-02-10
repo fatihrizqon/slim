40b7feeed4a0cadb57eac1950a3a4670c325ba62
fatihrizqon <fatihrizqon@gmail.com>
2018-02-10 12:23:03 +0700

    Add Materialize  Templating

--- app/Controllers/ExampleController.php
+++ app/Controllers/ExampleController.php
@@ -5,6 +5,9 @@ use App\Models\Example;
 
 class ExampleController extends BaseController{
     public function index($request, $response){
+      $this->container->view->render($response,'templates/header.twig');
+      $this->container->view->render($response,'templates/navbar.twig');
       $this->container->view->render($response,'home/home.twig');
+      $this->container->view->render($response,'templates/footer.twig');
     }
 }
